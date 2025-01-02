7c83a26648e3e8e799a1df6ad40abb38eeed5b15
char704 <nthanhphuong2112004@gmail.com>
2025-01-02 18:11:48 +0700

    Initial commit

---
+++ Library-Management-System-using-CSharp-main/LibraryManagementSystem/LibraryManagementSystem/GlobalConnection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem
+{
+    public static class GlobalConnection
+    {
+        // Chuỗi kết nối tĩnh
+        public static string ConnectionString = @"Data Source=DESKTOP-7VFC02H;Initial Catalog=library_management;Integrated Security=True";
+        public static string ImagesFolder = @"C:\Users\eoevo\OneDrive\Desktop\Library-Management-System-using-CSharp-main\Library-Management-System-using-CSharp-main\LibraryManagementSystem\LibraryManagementSystem\DashBoardImage";
+        public static string Books_Diretory = @"C:\Users\eoevo\OneDrive\Desktop\Library-Management-System-using-CSharp-main\Library-Management-System-using-CSharp-main\LibraryManagementSystem\LibraryManagementSystem\Books_Directory";
+        public static string Student_Directory = @"C:\Users\eoevo\OneDrive\Desktop\Library-Management-System-using-CSharp-main\Library-Management-System-using-CSharp-main\LibraryManagementSystem\LibraryManagementSystem\Student_Directory";
+    }
+
+
+}
